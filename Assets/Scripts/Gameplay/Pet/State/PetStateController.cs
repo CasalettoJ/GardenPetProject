89ee0ca3ff99aeae6f6326f9a9f49b038face28c
Jake Casaletto <Jake.m.casaletto@gmail.com>
2019-05-18 22:47:53 -0700

    talk bubble

--- Assets/Scripts/Gameplay/Pet/State/PetStateController.cs
+++ Assets/Scripts/Gameplay/Pet/State/PetStateController.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace PetProject
 {
     [RequireComponent(typeof(PetStats))]
     [RequireComponent(typeof(PetAbilities))]
+    [RequireComponent(typeof(NavMeshAgent))]
     public class PetStateController : StateController<PetStateController>
     {
         public PetStats Stats { get; private set; }
         public PetAbilities Abilities { get; private set; }
+        public NavMeshAgent NavMeshAgent { get; private set; }
 
         public override void Start()
         {
             base.Start();
             Stats = GetComponent<PetStats>();
             Abilities = GetComponent<PetAbilities>();
+            NavMeshAgent = GetComponent<NavMeshAgent>();
         }
     }
 }
